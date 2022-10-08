2e0100de7443990595d67e09cf7d3be53b06d5b2
AlexeyKurepin <kurepin@tut.by>
2022-10-08 22:57:24 +0300

    Create project and applications. Create users model.

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField('email',
+                              max_length=254,
+                              unique=True)
+
+    first_name = models.CharField('имя', max_length=150)
+    last_name = models.CharField('фамилия', max_length=150)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='пользователь'
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='автор'
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follower'
+            )
+        ]
+
+    def __str__(self):
+        return f'Подписка {self.user.username} на {self.author.username}.'
