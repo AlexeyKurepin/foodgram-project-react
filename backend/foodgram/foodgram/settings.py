f2eb70656464c7b13ac8c73222cb75449cbf555f
AlexeyKurepin <kurepin@tut.by>
2022-10-19 23:12:00 +0300

    Test workflow.

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='4ylfwd4@x)dq3-s*l+8g02#033n53^x)^2=!hrk46t@(&-376s')
+SECRET_KEY = os.getenv('SECRET_KEY',
+                       default='4ylfwd4@x)dq3-s*l+8g02#033n53^x)^2=!hrk46t@(&-376s')
 
 DEBUG = False
 
